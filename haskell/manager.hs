28c712ca9d8f62af2d0a3c30e6b66b40d672534c
Ana Beatriz <ana.beatriz.beckman.falcao@ccc.ufcg.edu.br>
2024-03-13 01:33:48 -0600

    part2

--- haskell/manager.hs
+++ haskell/manager.hs
@@ -25,5 +25,36 @@ instance Person Manager where
 instance Entity Manager where
     entityCpf manager = Manager.cpf manager
 
+instance Show Manager where
+    show(Meneger cpf name birth telephone address) = "\n¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨\n" ++
+                                                            "CPF: " ++ (show cpf) ++ "\n" ++
+                                                            "Nome: " ++ name ++ "\n" ++
+                                                            "Aniversário: " ++ (show birth) ++ "\n" ++
+                                                            "Telefone: " ++ (show telephone) ++
+                                                            "Endereço: " ++ address ++
+                                                      "\n¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨\n"
+
+instance Stringfy Manager where
+    toString(Meneger cpf ssn name birth telephone address) = show cpf ++ "," ++
+                                                                ssn ++ "," ++
+                                                                name ++ "," ++
+                                                                show birth ++ "," ++
+                                                                show telephone ++ "," ++
+                                                                address 
+
+-- divide a string de entrada por , usando o split/
+-- converte os campos para os tipos corretos e cria o valor manager
+-- retorno é uma tupla com os dados
+instance Read Meneger where                                                                
+     readsPrec _ str = do
+    let l = splitOn "," str
+    let cpf = read (l !! 0) :: ManagerCpf
+    let ssn = l !! 1
+    let name = l !! 2
+    let birth = l !! 3
+    let telephone = read (l !! 4) :: Telephone
+    let address = l !! 5
+    [(Manager cpf ssn name birth telephone address, "")]
+
 
 
