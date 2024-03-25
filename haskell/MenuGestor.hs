4ba6401d29b982ab945e50d2d5b323bedeba5b06
laysaOlivveira <laysa.santos.oliveira@ccc.ufcg.edu.br>
2024-03-25 15:55:49 -0300

    organizando opções

--- haskell/MenuGestor.hs
+++ haskell/MenuGestor.hs
@@ -19,12 +19,14 @@ menuGestor = do
     putStrLn "║   Escolha o assunto da funcionalidade      ║"
     putStrLn "║   de Gestor:                               ║"
     putStrLn "║                                            ║"
-    putStrLn "║   1. Gestor                                ║"
-    putStrLn "║   2. Funcionário                           ║"
-    putStrLn "║   3. Máquina                               ║"
-    putStrLn "║   4. Aluno                                 ║"
-    putStrLn "║   5. Financeiro                            ║"
-    putStrLn "║   6. Sair                                  ║"
+    putStrLn "║   [1]. Gestor                              ║"
+    putStrLn "║   [2]. Funcionário                         ║"
+    putStrLn "║   [3]. Máquina                             ║"
+    putStrLn "║   [4]. Aluno                               ║"
+    putStrLn "║   [5]. Financeiro                          ║"
+    putStrLn "║   [6]. Sair                                ║"
+    putStrLn "║                                            ║"
+    putStrLn "║   > Digite a opção:                        ║"
     putStrLn "╚════════════════════════════════════════════╝"
     opcao <- getLine
     case opcao of
@@ -42,15 +44,17 @@ menuGestorG = do
     putStrLn "╔════════════════════════════════════════════╗"
     putStrLn "║           Opções sobre Gestor:             ║"
     putStrLn "║                                            ║"
-    putStrLn "║   a. Consultar gestor                      ║"
-    putStrLn "║   b. Listar gestores                       ║"
-    putStrLn "║   c. Voltar para o menu                    ║"
+    putStrLn "║   [1]. Consultar gestor                    ║"
+    putStrLn "║   [2]. Listar gestores                     ║"
+    putStrLn "║   [3]. Voltar para o menu                  ║"
+    putStrLn "║                                            ║"
+    putStrLn "║   > Digite a opção:                        ║"
     putStrLn "╚════════════════════════════════════════════╝"
     opcaoGestorG <- getLine
     case opcaoGestorG of
-        "a" -> consultarGestor
-        "b" -> listarGestores
-        "c" -> main
+        "1" -> consultarGestor
+        "2" -> listarGestores
+        "3" -> main
         _ -> putStrLn "Opção inválida. Por favor, escolha novamente." >> menuGestor
 
 consultarGestor :: IO ()
@@ -74,18 +78,20 @@ menuFuncionarioG = do
     putStrLn "╔════════════════════════════════════════════╗"
     putStrLn "║           Opções sobre Funcionario:        ║"
     putStrLn "║                                            ║"
-    putStrLn "║   a. Criar funcionario                     ║"
-    putStrLn "║   b. Atualizar funcionario                 ║"
-    putStrLn "║   c. Listar funcionario                    ║"
-    putStrLn "║   d. Consultar funcionario                 ║"
-    putStrLn "║   e. Remover funcionario                   ║" 
-    putStrLn "║   f. Consultar funcionario                 ║"
-    putStrLn "║   g. Voltar para o menu                    ║"
+    putStrLn "║   [1]. Criar funcionario                   ║"
+    putStrLn "║   [2]. Atualizar funcionario               ║"
+    putStrLn "║   [3]. Listar funcionario                  ║"
+    putStrLn "║   [4]. Consultar funcionario               ║"
+    putStrLn "║   [5]. Remover funcionario                 ║" 
+    putStrLn "║   [6]. Consultar funcionario               ║"
+    putStrLn "║   [7]. Voltar para o menu                  ║"
+    putStrLn "║                                            ║"
+    putStrLn "║   > Digite a opção:                        ║"
     putStrLn "╚════════════════════════════════════════════╝"
     opcaoFuncionarioG <- getLine
     case opcaoFuncionarioG of
-        "a" -> -- nome das funcoes
-        "f" -> main
+        "1" -> -- nome das funcoes
+        "7" -> main
         _ -> putStrLn "Opção inválida. Por favor, escolha novamente." >> menuFuncionarioG
 
 
@@ -94,20 +100,22 @@ menuMaquinaG = do
     putStrLn "╔═══════════════════════════════════════════════════════╗"
     putStrLn "║           Opções sobre Máquina:                       ║"
     putStrLn "║                                                       ║"
-    putStrLn "║   a. Criar máquina                                    ║"
-    putStrLn "║   b. Verificar datas de manutenção dos equipamentos   ║"
-    putStrLn "║   c. Listar equipamentos cadastrados                  ║"
-    putStrLn "║   d. Adicionar máquina com necessidade de reparo      ║"
-    putStrLn "║   e. Listar máquinas com necessidades de reparo       ║"
-    putStrLn "║   f. Verificar quantidade de máquinas cadastradas     ║"
-    putStrLn "║   g. Voltar ao menu principal                         ║"
+    putStrLn "║   [1]. Criar máquina                                  ║"
+    putStrLn "║   [2]. Verificar datas de manutenção dos equipamentos ║"
+    putStrLn "║   [3]. Listar equipamentos cadastrados                ║"
+    putStrLn "║   [4]. Adicionar máquina com necessidade de reparo    ║"
+    putStrLn "║   [5]. Listar máquinas com necessidades de reparo     ║"
+    putStrLn "║   [6]. Verificar quantidade de máquinas cadastradas   ║"
+    putStrLn "║   [7]. Voltar ao menu principal                       ║"
+    putStrLn "║                                                       ║"
+    putStrLn "║   > Digite a opção:                                   ║"
     putStrLn "╚═══════════════════════════════════════════════════════╝"
     opcaoMaquinaG <- getLine
     case opcaoMaquinaG of 
-      "a" -> criarMaquinas
-      "b" -> listarMaquinasOrdemAlfabetica 
-      "c" -> listarEquipamentos
-      "d" -> do
+      "1" -> criarMaquinas
+      "2" -> listarMaquinasOrdemAlfabetica 
+      "3" -> listarEquipamentos
+      "4" -> do
         putStrLn "Informe o ID: "
         id <- getLine
         putStrLn "Informe o nomeG: "
@@ -115,12 +123,12 @@ menuMaquinaG = do
         putStrLn "Informe a data de manutenção: "
         dataMan <- getLine
         adicionarMaquinaReparo (Maquina (show id) nomeG dataMan) >> menuMaquina
-      "e" -> imprimirMaquinasReparo "maquina_reparo.txt" >> menuMaquina
-      "f" -> do
+      "5" -> imprimirMaquinasReparo "maquina_reparo.txt" >> menuMaquina
+      "6" -> do
         numeroMaquinas <- contarMaquinas "maquina.txt"
         putStrLn $ "Número de máquinas registradas: " ++ show numeroMaquinas
         menuMaquina
-      "g" -> main
+      "7" -> main
       _ -> putStrLn "Opção inválida. Por favor, escolha novamente." >> menuMaquinaG
 
 criarMaquinas :: IO ()
@@ -140,15 +148,17 @@ menuFinanceiroG = do
     putStrLn "╔════════════════════════════════════════════╗"
     putStrLn "║           Opções sobre Financeiro:         ║"
     putStrLn "║                                            ║"
-    putStrLn "║   a. Folha de Pagamento do funcionário     ║"
-    putStrLn "║   b. Renda e Gastos Mensais                ║"
-    putStrLn "║   c. Voltar para o menu                    ║"
+    putStrLn "║   [1]. Folha de Pagamento do funcionário   ║"
+    putStrLn "║   [2]. Renda e Gastos Mensais              ║"
+    putStrLn "║   [3]. Voltar para o menu                  ║"
+    putStrLn "║                                            ║"
+    putStrLn "║   > Digite a opção:                        ║"
     putStrLn "╚════════════════════════════════════════════╝"
     opcaoAlunoG <- getLine
     case opcaoAlunoG of
-        "a" -> folhaDePagamento
-        "b" -> ----
-        "c" -> main
+        "1" -> folhaDePagamento
+        "2" -> ----
+        "3" -> main
         _ -> putStrLn "Opção inválida. Por favor, escolha novamente." >> menuFinanceiroG
 
 
